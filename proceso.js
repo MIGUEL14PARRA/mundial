49535134fb33bc882c1b6737cf7ea880a2a23e64
MIGUEL14PARRA <parramiguel378@gmail.com>
2023-06-29 19:19:38 -0500

    first commit

---
+++ proceso.js
@@ -0,0 +1,25 @@
+function tico() {
+    document.getElementById('#').innerHTML= ""
+    +"<div><ul><li>Estadio Mineirão (Belo Horizonte)</li></ul></div><br>"
+    +"<div><ul><li>El Estadio Mineirão es un estadio deportivo ubicado en la ciudad brasileña de Belo Horizonte, capital del estado de Minas Gerais. En este estadio hace de local Cruzeiro, además fue sede del Mundial de Brasil 2014. Hasta la temporada 2022, el Atlético Mineiro también disputó sus partidos en el estadio.</li></ul></div> ";
+}
+function tica() {
+    document.getElementById('#').innerHTML= ""
+    +"<div><ul><li>Estadio Nacional (Brasilia)</li></ul></div><br>"
+    +"El Estadio Nacional de Brasilia Mané Garrincha es un estadio multiusos ubicado en Brasília en el Distrito Federal en Brasil. Fue inaugurado en 1974 y tras la última remodelación, de 2013, cuenta con una capacidad de 72 800 espectadores sentados.";
+ }
+function ticy() {
+    document.getElementById('#').innerHTML= ""
+    +"Arena Pantanal (Cuiabá)<br> <br>"
+    +"El Arena Pantanal es un estadio multipropósito en la ciudad de Cuiabá, estado de Mato Grosso en Brasil, fue construido para albergar cuatro partidos de la Copa Mundial de Fútbol de 2014 y está emplazado en los terrenos donde se erigía el antiguo estadio José Fragelli, demolido en el año 2010.";
+ }
+ function ticu() {
+    document.getElementById('#').innerHTML= ""
+    +"Estadio Centenario<br> <br>"
+    +" Es el estadio con mayor capacidad de Uruguay y uno de los quince más grandes de América.4​ El 18 de julio de 1983 fue declarado por la FIFA como Monumento Histórico del Fútbol Mundial, siendo la única construcción de esta índole en todo el mundo.";
+ }
+ function ticb() {
+    document.getElementById('#').innerHTML= ""
+    +"Estadio Manuel Murillo Toro <br> <br>"
+    +"El Manuel Murillo Toro es un escenario deportivo ubicado en la ciudad de Ibagué departamento del Tolima a 1165 m s. n. m.. Con sus últimas remodelaciones, el estadio tiene capacidad de aforo para 28 100 espectadores. Su uso principal es para el fútbol, en donde juega como local el equipo Deportes Tolima";
+ }
